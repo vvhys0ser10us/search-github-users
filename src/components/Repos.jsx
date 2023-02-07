43d7d640b53063e50b1713303770fe4815f38055
kens0serious <kens0serious@gmail.com>
2023-02-07 17:18:36 -0500

    calculated top 5 used language data for charts

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
+import { useGlobalContext } from '../context/context'
 
 const Repos = () => {
+  const { gitRepos } = useGlobalContext()
+
+  //calculate langauge occurrence
+  let data = gitRepos.reduce((total, current) => {
+    if (!current.language) return total
+    total[current.language] = total[current.language] ?? {
+      label: current.language,
+      value: 0,
+    }
+    total[current.language] = {
+      ...total[current.language],
+      value: total[current.language].value + 1,
+    }
+    return total
+  }, {})
+  // keep top 5 used language
+  data = Object.values(data)
+    .sort((a, b) => b.value - a.value)
+    .slice(0, 5)
+
   return <div>Repos</div>
 }
 
