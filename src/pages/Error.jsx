f4c6d3914802999ad30be91cf8bcee958c40c17b
kens0serious <kens0serious@gmail.com>
2023-02-06 22:48:42 -0500

    finished error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
+import error from '../images/error_image.jpg'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
-  return <div>Error</div>
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>page not found</h3>
+        <Link to={'/'} className="btn">
+          Back Home
+        </Link>
+      </div>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: var(--clr-primary-10);
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    color: var(--clr-grey-3);
+    margin-bottom: 1.5em;
+  }
+`
 
 export default Error
