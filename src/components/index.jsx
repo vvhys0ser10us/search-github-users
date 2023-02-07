a51fea0a1a21193df04136732e5e1ba899423e0e
kens0serious <kens0serious@gmail.com>
2023-02-06 23:35:50 -0500

    added dashboard components

---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+import Info from './Info'
+import Navbar from './Navbar'
+import Repos from './Repos'
+import Search from './Search'
+import User from './User'
+
+export { Info, Navbar, Repos, Search, User }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { User, Repos, Search, Navbar, Info } from '../components'
 
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <main>
+      <Navbar></Navbar>
+      <Search></Search>
+      <Info></Info>
+      <User></User>
+      <Repos></Repos>
+    </main>
+  )
 }
 
 export default Dashboard
